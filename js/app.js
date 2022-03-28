f9c55655bb10eed8f8659c99945bdce9fcf8d62c
SantiCastelli <castellist@gmail.com>
2022-03-28 09:41:23 +0200

    terminado

--- js/app.js
+++ js/app.js
@@ -4,3 +4,30 @@ $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
 });
+
+$("#add").click(function(){
+    counter++;
+    $("#counter").text(counter);
+    color();
+});
+
+$("#low").click(function(){
+    counter--;
+    $("#counter").text(counter);
+    color();
+});
+
+function color() {
+   if(counter < 0) {
+    $("#counter").addClass("negativo");
+   }
+   else if (counter > 0){
+    $("#counter").addClass("positivo");
+    $("#counter").removeClass("negativo");
+   }
+   else {
+    $("#counter").removeClass("negativo");
+    $("#counter").removeClass("positivo");
+   }
+    
+}
